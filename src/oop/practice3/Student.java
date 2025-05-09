320dab47af4864d520383d771cd37f94e3fa031b
Daniel Bustamante <daniel.bus.cor@gmail.com>
2025-05-09 19:07:05 -0400

    Added the generateStudentId method to the student class in tha oop practice 3 package

--- src/oop/practice3/Student.java
+++ src/oop/practice3/Student.java
@@ -4,14 +4,22 @@ public class Student {
     private String name;
     private int age;
     private double grade;
+    private int studentId;
 
     public Student(String name, int age, double grade) {
         this.name = name;
         this.age = age;
         this.grade = grade;
+        this.studentId = generateStudentId();
+    }
+
+    public int generateStudentId() {
+        int studentID =  (int) (Math.random() * 1000);
+        return studentID;
     }
 
     public void displayInformation() {
-        System.out.println("Student name: " + this.name + "\nAge: " + this.age + "\n Grade: " + this.grade);
+        System.out.println("Student name: " + this.name + "\nAge: " + this.age + "\n Grade: " + this.grade +
+                "\nStudent Id: " + this.studentId);
     }
 }
